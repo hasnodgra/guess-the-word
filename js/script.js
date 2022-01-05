674db404b01ac969fa27dabafe36684f338105ce
hasnodgra <hasnodgra@gmail.com>
2022-01-05 17:16:02 -0500

    pt.1 with notes

--- js/script.js
+++ js/script.js
@@ -8,25 +8,24 @@ const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
-const word = "magnolia";
+const word = "magnolia"; //this will serve as a test word until API later
 
 //letter placeholders for guessing
-const placeholder = function (word) {
+const placeholder = function (word) { //create a fucntion to upate <p>'s inner text for "words-in-progress" element
     const placeholderLetters = [];
     for (const letter of word) {
         console.log(letter);
-        placeholderLetters.push("●");
+        placeholderLetters.push("●"); //push adds new items to array
     }
     wordInProgress.innerText = placeholderLetters.join("");
 }; //an array that was joined back to a string
 
-placeholder(word);
+placeholder(word); //"call the function and pass it the word"
 
-//button
-
+//event listener for the button
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault(); //stops form's reloading behavior
-    const guess = letterInput.value; 
-    console.log(guess);
-    letterInput.value="";
+    const guess = letterInput.value; //create variable to capture value of input
+    console.log(guess); //log out the value capturing the input
+    letterInput.value=""; //empty the value of the inputs
 });
